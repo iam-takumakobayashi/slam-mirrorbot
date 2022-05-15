9d06b242836fe48446069ddb65e021e3a707f202
iam-takumakobayashi <105575313+iam-takumakobayashi@users.noreply.github.com>
2022-05-15 20:06:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,64 +1,64 @@
-wget -qO /usr/src/app/alive.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/alive.py
-wget -qO /usr/src/app/aria.bat https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/aria.bat
-wget -qO /usr/src/app/driveid.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/driveid.py
-wget -qO /usr/src/app/nodes.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/nodes.py
-wget -qO /usr/src/app/qBittorrent.conf https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/qBittorrent.conf
-wget -qO /usr/src/app/wserver.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/wserver.py
-
-wget -qO /usr/src/app/bot/__init__.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/__init__.py
-wget -qO /usr/src/app/bot/__main__.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/__main__.py
-
-wget -qO /usr/src/app/bot/helper/ext_utils/bot_utils.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/bot_utils.py
-wget -qO /usr/src/app/bot/helper/ext_utils/custom_filters.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/custom_filters.py
-wget -qO /usr/src/app/bot/helper/ext_utils/db_handler.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/db_handler.py
-wget -qO /usr/src/app/bot/helper/ext_utils/exceptions.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/exceptions.py
-wget -qO /usr/src/app/bot/helper/ext_utils/fs_utils.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/fs_utils.py
-wget -qO /usr/src/app/bot/helper/ext_utils/shortenurl.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/shortenurl.py
-
-wget -qO /usr/src/app/bot/helper/mirror_utils/download_utils/aria2_download.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/aria2_download.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/download_utils/direct_link_generator.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/direct_link_generator.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/download_utils/download_helper.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/download_helper.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/download_utils/mega_downloader.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/mega_downloader.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/download_utils/qbit_downloader.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/qbit_downloader.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/download_utils/telegram_downloader.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/telegram_downloader.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
-
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/aria_download_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/aria_download_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/clone_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/clone_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/extract_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/extract_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/gdownload_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/gdownload_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/listeners.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/listeners.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/mega_download_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/mega_download_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/qbit_download_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/qbit_download_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/split_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/split_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/tar_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/tar_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/telegram_download_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/telegram_download_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/tg_upload_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/tg_upload_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/upload_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/upload_status.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/status_utils/youtube_dl_download_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/youtube_dl_download_status.py
-
-wget -qO /usr/src/app/bot/helper/mirror_utils/upload_utils/gdriveTools.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/upload_utils/gdriveTools.py
-wget -qO /usr/src/app/bot/helper/mirror_utils/upload_utils/pyrogramEngine.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
-
-wget -qO /usr/src/app/bot/helper/telegram_helper/bot_commands.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/telegram_helper/bot_commands.py
-wget -qO /usr/src/app/bot/helper/telegram_helper/button_build.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/telegram_helper/button_build.py
-wget -qO /usr/src/app/bot/helper/telegram_helper/filters.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/telegram_helper/filters.py
-wget -qO /usr/src/app/bot/helper/telegram_helper/message_utils.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/telegram_helper/message_utils.py
-
-wget -qO /usr/src/app/bot/modules/authorize.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/authorize.py
-wget -qO /usr/src/app/bot/modules/cancel_mirror.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/cancel_mirror.py
-wget -qO /usr/src/app/bot/modules/clone.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/clone.py
-wget -qO /usr/src/app/bot/modules/count.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/count.py
-wget -qO /usr/src/app/bot/modules/delete.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/delete.py
-wget -qO /usr/src/app/bot/modules/eval.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/eval.py
-wget -qO /usr/src/app/bot/modules/leech_settings.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/leech_settings.py
-wget -qO /usr/src/app/bot/modules/list.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/list.py
-wget -qO /usr/src/app/bot/modules/mirror.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/mirror.py
-wget -qO /usr/src/app/bot/modules/mirror_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/mirror_status.py
-wget -qO /usr/src/app/bot/modules/shell.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/shell.py
-wget -qO /usr/src/app/bot/modules/speedtest.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/speedtest.py
-wget -qO /usr/src/app/bot/modules/torrent_search.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/torrent_search.py
-wget -qO /usr/src/app/bot/modules/watch.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/watch.py
+wget -qO /alive.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/alive.py
+wget -qO /aria.bat https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/aria.bat
+wget -qO /driveid.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/driveid.py
+wget -qO /nodes.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/nodes.py
+wget -qO /qBittorrent.conf https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/qBittorrent.conf
+wget -qO /wserver.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/wserver.py
+		 /
+wget -qO /bot/__init__.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/__init__.py
+wget -qO /bot/__main__.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/__main__.py
+		 /
+wget -qO /bot/helper/ext_utils/bot_utils.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/bot_utils.py
+wget -qO /bot/helper/ext_utils/custom_filters.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/custom_filters.py
+wget -qO /bot/helper/ext_utils/db_handler.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/db_handler.py
+wget -qO /bot/helper/ext_utils/exceptions.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/exceptions.py
+wget -qO /bot/helper/ext_utils/fs_utils.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/fs_utils.py
+wget -qO /bot/helper/ext_utils/shortenurl.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/ext_utils/shortenurl.py
+		 /
+wget -qO /bot/helper/mirror_utils/download_utils/aria2_download.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/aria2_download.py
+wget -qO /bot/helper/mirror_utils/download_utils/direct_link_generator.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/direct_link_generator.py
+wget -qO /bot/helper/mirror_utils/download_utils/download_helper.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/download_helper.py
+wget -qO /bot/helper/mirror_utils/download_utils/mega_downloader.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/mega_downloader.py
+wget -qO /bot/helper/mirror_utils/download_utils/qbit_downloader.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/qbit_downloader.py
+wget -qO /bot/helper/mirror_utils/download_utils/telegram_downloader.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/telegram_downloader.py
+wget -qO /bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
+		 /
+wget -qO /bot/helper/mirror_utils/status_utils/aria_download_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/aria_download_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/clone_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/clone_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/extract_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/extract_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/gdownload_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/gdownload_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/listeners.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/listeners.py
+wget -qO /bot/helper/mirror_utils/status_utils/mega_download_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/mega_download_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/qbit_download_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/qbit_download_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/split_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/split_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/status.py
+wget -qO /bot/helper/mirror_utils/status_utils/tar_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/tar_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/telegram_download_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/telegram_download_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/tg_upload_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/tg_upload_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/upload_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/upload_status.py
+wget -qO /bot/helper/mirror_utils/status_utils/youtube_dl_download_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/status_utils/youtube_dl_download_status.py
+		 /
+wget -qO /bot/helper/mirror_utils/upload_utils/gdriveTools.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/upload_utils/gdriveTools.py
+wget -qO /bot/helper/mirror_utils/upload_utils/pyrogramEngine.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+		 /
+wget -qO /bot/helper/telegram_helper/bot_commands.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/telegram_helper/bot_commands.py
+wget -qO /bot/helper/telegram_helper/button_build.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/telegram_helper/button_build.py
+wget -qO /bot/helper/telegram_helper/filters.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/telegram_helper/filters.py
+wget -qO /bot/helper/telegram_helper/message_utils.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/helper/telegram_helper/message_utils.py
+		 /
+wget -qO /bot/modules/authorize.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/authorize.py
+wget -qO /bot/modules/cancel_mirror.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/cancel_mirror.py
+wget -qO /bot/modules/clone.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/clone.py
+wget -qO /bot/modules/count.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/count.py
+wget -qO /bot/modules/delete.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/delete.py
+wget -qO /bot/modules/eval.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/eval.py
+wget -qO /bot/modules/leech_settings.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/leech_settings.py
+wget -qO /bot/modules/list.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/list.py
+wget -qO /bot/modules/mirror.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/mirror.py
+wget -qO /bot/modules/mirror_status.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/mirror_status.py
+wget -qO /bot/modules/shell.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/shell.py
+wget -qO /bot/modules/speedtest.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/speedtest.py
+wget -qO /bot/modules/torrent_search.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/torrent_search.py
+wget -qO /bot/modules/watch.py https://github.com/iam-takumakobayashi/slam-mirrorbot/raw/master/bot/modules/watch.py
 
 ./aria.sh; python3 -m bot
